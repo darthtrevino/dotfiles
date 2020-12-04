7efdeecbdf12be6a8698c59052cc527a12bf9e8e
Chris Trevino <darthtrevino@users.noreply.github.com>
2020-12-04 11:50:19 -0800

    Update 2-noadmin.ps1

--- windows-setup/2-noadmin.ps1
+++ windows-setup/2-noadmin.ps1
@@ -7,6 +7,10 @@ invoke-webrequest https://wslstorestorage.blob.core.windows.net/wslblob/wsl_upda
 Start-Process -wait ~/Downloads/wsl_update_x64.msi
 wsl --set-default-version 2
 
+#
+mkdir c:\ws
+ssh-keygen
+
 # Make sure winget is available -> update from Windows store with corp&personal acct.
 winget install git
 winget install dotnet
@@ -24,7 +28,6 @@ winget install Microsoft.AzureStorageExplorer
 winget install Microsoft.AzureCLI
 winget install Microsoft.EdgeDev
 winget install Ubuntu
-winget install docker
 winget install Microsoft.EdgeDev
 
 nvm install 14
